18563c8e4c916380d5915ffc87b1a1d488a28dfd
MichaelRoss2023 <61289960+MichaelRoss2023@users.noreply.github.com>
2022-09-30 17:54:02 -0700

    added log out and temp navbar changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,12 @@
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
 
+    const { logout } = useAuth0();
     var loggedin = false;
 
     if(sessionStorage.getItem("email") != '')
@@ -18,9 +21,7 @@ function Navbar() {
                 <span className="npo-heading">Girard Training Stables</span>
                 <span className="filler"></span>
                 <ul className="links">
-                <li>
-                    Log Out
-                </li>
+                <Button color="primary" variant="contained" onClick={() => logout({ returnTo: "https://girard-client.herokuapp.com" })} className="button" id="Log In Button">Log Out</Button>
                 <li>
                     <Link to="/volunteer" style={{ textDecoration: 'none' }}>Calendar</Link>
                 </li>
@@ -47,6 +48,11 @@ function Navbar() {
                 <li>
                     <Link to="/" style={{ textDecoration: 'none' }}>Sign Up</Link>
                 </li>
+                <li>
+                    <Link to="/volunteer" style={{ textDecoration: 'none' }}>Calendar</Link>
+                </li>
+                <Button color="primary" variant="contained" onClick={() => logout({ returnTo: "https://girard-client.herokuapp.com" })} className="button" id="Log In Button">Log Out</Button>
+
                 </ul>
             </div>
         </div>
