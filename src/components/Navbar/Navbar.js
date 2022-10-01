13fabcb74274b282a37f998b4d575f9fce4bb808
MichaelRoss2023 <61289960+MichaelRoss2023@users.noreply.github.com>
2022-09-30 21:30:10 -0700

    Minor changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
 
-    const { logout } = useAuth0();
+    const { logout, user, isAuthenticated, isLoading } = useAuth0();
 
         return  (
             isAuthenticated && (
